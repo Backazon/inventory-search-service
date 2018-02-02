a2279cbc5a9fa40f76b4ea6ab4ccabcace58b5dc
kayfo23 <kayleigh.s.foley@gmail.com>
2018-02-01 19:06:57 -0800

    Refactor db connection setup

--- databases/mongo-inventory/index.js
+++ databases/mongo-inventory/index.js
@@ -1,37 +1,66 @@
 const mongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://localhost:27017/backazon';
+// const url = 'mongodb://localhost:27017/backazon';
 
 
-//will be called server-side to get item details
-const findItemById = function(itemId) {
+// //will be called server-side to get item details
+// const findItemById = function(itemId) {
 
-  mongoClient.connect(url, function(err, db) {
-    console.log('Connected to MongoDB - Backazon');
+//   mongoClient.connect(url, function(err, db) {
+//     console.log('Connected to MongoDB - Backazon');
 
-    var col = db.collection('inventory');
+//     var col = db.collection('inventory');
   
-    col.find({ item_id: itemId })
+//     col.find({ item_id: itemId })
+//   })
+
+// }
+
+// //will be called server-side once for each department 
+// const pullTrendingItems = function(department) {
+
+//   mongoClient.connect(url, function (err, db) {
+//     console.log('Connected to MongoDB - Backazon');
+
+//     var col = db.collection('inventory');
+
+//     col.find({ department: department }).limit(100)
+//   })
+
+// }
+
+
+var state = {
+  db: null,
+}
+
+exports.connect = function(url, done) {
+  if (state.db) return done()
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) return done(err)
+    state.db = db
+    done()
   })
-
 }
 
-//will be called server-side once for each department 
-const pullTrendingItems = function(department) {
+exports.get = function() {
+  return state.db
+}
 
-  mongoClient.connect(url, function (err, db) {
-    console.log('Connected to MongoDB - Backazon');
-
-    var col = db.collection('inventory');
-
-    col.find({ department: department }).limit(100)
-  })
-
+exports.close = function(done) {
+  if (state.db) {
+    state.db.close(function(err, result) {
+      state.db = null
+      state.mode = null
+      done(err)
+    })
+  }
 }
 
 
 
-module.exports = {
-  findItemById,
-  pullTrendingItems
-}
+// module.exports = {
+//   findItemById,
+//   pullTrendingItems
+// }
