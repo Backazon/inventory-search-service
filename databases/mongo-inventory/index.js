314001135441f340e0b6c161a282d11828b73c1c
kayfo23 <kayleigh.s.foley@gmail.com>
2018-02-02 18:55:09 -0800

    Set up /details & /newitem inventory endpoints

--- databases/mongo-inventory/index.js
+++ databases/mongo-inventory/index.js
@@ -1,64 +1,64 @@
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
 
-// const url = 'mongodb://localhost:27017/backazon';
+// // const url = 'mongodb://localhost:27017/backazon';
 
 
-// //will be called server-side to get item details
-// const findItemById = function(itemId) {
+// // //will be called server-side to get item details
+// // const findItemById = function(itemId) {
 
-//   mongoClient.connect(url, function(err, db) {
-//     console.log('Connected to MongoDB - Backazon');
+// //   mongoClient.connect(url, function(err, db) {
+// //     console.log('Connected to MongoDB - Backazon');
 
-//     var col = db.collection('inventory');
+// //     var col = db.collection('inventory');
   
-//     col.find({ item_id: itemId })
-//   })
+// //     col.find({ item_id: itemId })
+// //   })
 
+// // }
+
+// // //will be called server-side once for each department 
+// // const pullTrendingItems = function(department) {
+
+// //   mongoClient.connect(url, function (err, db) {
+// //     console.log('Connected to MongoDB - Backazon');
+
+// //     var col = db.collection('inventory');
+
+// //     col.find({ department: department }).limit(100)
+// //   })
+
+// // }
+
+
+// var state = {
+//   db: null,
 // }
 
-// //will be called server-side once for each department 
-// const pullTrendingItems = function(department) {
+// exports.connect = function(url, done) {
+//   if (state.db) return done()
 
-//   mongoClient.connect(url, function (err, db) {
-//     console.log('Connected to MongoDB - Backazon');
-
-//     var col = db.collection('inventory');
-
-//     col.find({ department: department }).limit(100)
+//   MongoClient.connect(url, function(err, db) {
+//     if (err) return done(err)
+//     state.db = db
+//     done()
 //   })
-
 // }
 
+// exports.get = function() {
+//   var collection = state.db.getCollection('inventory')
+//   console.log(collection)
+//   return state.db
+// }
 
-var state = {
-  db: null,
-}
-
-exports.connect = function(url, done) {
-  if (state.db) return done()
-
-  MongoClient.connect(url, function(err, db) {
-    if (err) return done(err)
-    state.db = db
-    done()
-  })
-}
-
-exports.get = function() {
-  var collection = state.db.getCollection('inventory')
-  console.log(collection)
-  return state.db
-}
-
-exports.close = function(done) {
-  if (state.db) {
-    state.db.close(function(err, result) {
-      state.db = null
-      state.mode = null
-      done(err)
-    })
-  }
-}
+// exports.close = function(done) {
+//   if (state.db) {
+//     state.db.close(function(err, result) {
+//       state.db = null
+//       state.mode = null
+//       done(err)
+//     })
+//   }
+// }
 
 
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 
 const express = require('express')
 const app = express()
+const assert = require('assert')
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 //INVENTORY DATABASE
 // const inventoryDb = require('../databases/mongo-inventory/index.js')
 const MongoClient = require('mongodb').MongoClient
-var inventory
+var db, inventory
+// const departments = require('../databases/mongo-inventory/dataGenerator.js')
 
 //connect to MongoDB on start
 MongoClient.connect('mongodb://localhost:27017/backazon', (err, client) => {
@@ -13,7 +18,8 @@ MongoClient.connect('mongodb://localhost:27017/backazon', (err, client) => {
     console.log('Unable to connect to Mongo')
     process.exit(1)
   } else {
-    inventory = client.db('backazon').collection('inventory')
+    db = client.db('backazon')
+    inventory = db.collection('inventory')
 
     app.listen(3000, function() {
       console.log('Listening on port 3000...')
@@ -32,20 +38,35 @@ GET request to '/trending', when client visits Backazon homepage
       [ summarized item objects ]
     }
 */
-app.get('/trending', (req, res) => {
+app.get('/trending', async (req, res) => {
   //get list of all departments
   //iterate over departments and query for top 100 items in each dept
-
   var trendingItems = []
   
-  inventory.find({ item_id: 1504401 }).limit(100).toArray(function (err, result) {
-    if (err) throw err
+  function getTrendingItems() {
+    for (var i = 0; i < departments.length; i++) {
+      inventory.find({ department: departments[i] }).sort({ avg_rating: 1 }).limit(100).toArray(function (err, result) {
+        if (err) { 
+          throw err
+        } else {
+          console.log(result);
+          trendingItems.push(result);
+        }
+      })
+    }
+  }
+  try {
+    await getTrendingItems().then(() => {
+      console.log(trendingItems)
+      res.send(trendingItems);
+    })
+  } catch (err) {
+    console.log(err)
+  }
 
-    console.log(result)
-  })
-
-
-  res.sendStatus(200)
+  //send back trending items
+  // res.status(201).send(trendingItems)
+  // res.sendStatus(200)
 })
 
 /*
@@ -72,11 +93,19 @@ TODO: send update to user analytics, format:
     Timestamp : dateTime
   }
 */
-app.get('/details', function(req, res) {
-  //get item id from req object
+app.get('/details', (req, res) => {
 
-  var collection = inventoryDb.get().collection('inventory')
-  // collection.find({ item_id: })
+  var itemId = parseInt(req.query.item_id)
+  
+  inventory.findOne({item_id: itemId}, (err, doc) => {
+    if (err) res.status(400).json('Could not find item')
+
+    assert.equal(null, err)
+    assert.ok(doc != null)
+
+    res.status(201).send(doc)
+  })
+
 })
 
 /*
@@ -88,7 +117,32 @@ POST request to '/newitem', when client submits new item to be hosted on Backazo
     }
   Response status: 200
 */
+app.post('/newitem', (req, res) => {
 
+  var newItem = {
+    item_id: parseInt(req.body.item_id),
+    name: req.body.name,
+    description: req.body.description,
+    price: parseInt(req.body.price),
+    color: req.body.color,
+    size: req.body.size,
+    inventory: 1,
+    avg_rating: 0,
+    review_count: 0,
+    image_url: req.body.image_url,
+    category: req.body.category,
+    subcategory: req.body.subcategory,
+    department: req.body.department,
+    creation_date: new Date()
+  }
+
+  inventory.insertOne(newItem, (err, result) => {
+    assert.equal(null, err)
+    assert.equal(1, result.insertedCount)
+
+    res.status(201).send('New item successfully added')
+  })
+})
 
 /*
 TODO: confirm data object with Chase
